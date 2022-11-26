3404f0d9a789b04d3a63ea1c4509df7863c9c390
Slobozhancky <46904434+Slobozhancky@users.noreply.github.com>
2022-11-26 22:23:54 +0200

    feat: add .json config and watch mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import counter from "./counter";
 import "./styles/style.css";
 
+import data from "./assets/data.json";
+
 const body = document.querySelector("body");
 const btn = document.createElement("button");
 let counterTitle = document.querySelector(".counter_title");
 
+console.log(data);
+
 btn.classList.add("btn");
 btn.innerHTML = "Click me";
-git 
 let count = 0;
 function getRes() {
     count = counter(count);
