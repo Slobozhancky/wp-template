8735bae029b132a0ea29b56b70879607711e60bc
Slobozhancky <46904434+Slobozhancky@users.noreply.github.com>
2022-11-27 09:33:14 +0200

    feat: add config for images

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@ import counter from "./counter";
 import "./styles/style.css";
 
 import data from "./assets/data.json";
+import Img from "./assets/images/img.jpg";
 
 const body = document.querySelector("body");
 const btn = document.createElement("button");
 let counterTitle = document.querySelector(".counter_title");
 
-console.log(data);
-
-btn.classList.add("btn");
-btn.innerHTML = "Click me";
 let count = 0;
 function getRes() {
     count = counter(count);
     counterTitle.innerHTML = count;
 }
 
+btn.classList.add("btn");
+btn.innerHTML = "Click me";
+
 btn.addEventListener("click", () => getRes());
 
 body.appendChild(btn);
+
+const img = new Image();
+img.src = Img;
+
+body.appendChild(img);
+
+console.log(img);
